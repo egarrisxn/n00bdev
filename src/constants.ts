963a802a188be6d342a96ee6af8aa262c608e907
EGARRISXN <egarrisxn@gmail.com>
2024-09-29 16:29:42 -0400

    metadata

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const SITE_TITLE = 'n00b:dev';
-export const SITE_DESCRIPTION =
-  'Tips & Tricks For The New Developer';
+export const SITE_TITLE = "n00b:dev";
+export const SITE_DESCRIPTION = "Tips & Tricks For The New Developer";
+export const OG_IMAGE = "https://n00bdev.vercel.app/images/og-image.png";
 export const HOMEPAGE_POST_LIMIT = 6;
 export const POSTS_PER_PAGE = 6;
